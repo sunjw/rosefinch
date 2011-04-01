48114eba7e7da3b0df75972ab50292df719f101a
sunjw8888 <sunjw8888@0e1cc27d-1b58-4253-b746-06118f85d288>
2011-04-01 08:28:39 +0000

    忽略配置文件
    git-svn-id: https://rosefinch.svn.sourceforge.net/svnroot/rosefinch@90 0e1cc27d-1b58-4253-b746-06118f85d288

--- trunk/phpfm/admin/database.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// 数据库参数 Database parameters
-define("DB_USER", "rosefinch");
-define("DB_PSWD", "rose");
-define("DB_NAME", "rosefinch");
-define("DB_HOST", "localhost");
-
-?>
--- trunk/phpfm/admin/usermng.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// 权限 privilege
-define("ROSE_BROWSER", 0); // 浏览
-define("ROSE_MODIFY", 100); // 修改文件
-define("ROSE_ADMIN", 100); // 全部管理
-
-?>
