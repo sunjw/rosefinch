a1f5afe57ab31980a9798972a1bb91600fbd6d6f
sunjw8888 <sunjw8888@0e1cc27d-1b58-4253-b746-06118f85d288>
2011-12-15 02:25:46 +0000

    修正 bug
    git-svn-id: https://rosefinch.svn.sourceforge.net/svnroot/rosefinch@108 0e1cc27d-1b58-4253-b746-06118f85d288

--- trunk/phpfm/func/post.func.php
+++ trunk/phpfm/func/post.func.php
@@ -3,6 +3,10 @@ require_once "../inc/defines.inc.php";
 require_once "../inc/common.inc.php";
 require_once "../inc/post.class.php";
 
+// For uploadify
+if(isset($_REQUEST['session']) && $_REQUEST['session']) 
+	@session_id($_REQUEST['session']);
+
 @session_start();
 
 $post = new Post(post_query("oper"));
