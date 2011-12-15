773308df7751963848af3a28d45fe0962d6685c6
sunjw8888 <sunjw8888@0e1cc27d-1b58-4253-b746-06118f85d288>
2011-12-15 02:11:53 +0000

    修正关于 session 的 bug
    git-svn-id: https://rosefinch.svn.sourceforge.net/svnroot/rosefinch@107 0e1cc27d-1b58-4253-b746-06118f85d288

--- trunk/phpfm/func/post.func.php
+++ trunk/phpfm/func/post.func.php
@@ -3,10 +3,6 @@ require_once "../inc/defines.inc.php";
 require_once "../inc/common.inc.php";
 require_once "../inc/post.class.php";
 
-if($_REQUEST['session']) 
-	session_id($_REQUEST['session']);
-
-
 @session_start();
 
 $post = new Post(post_query("oper"));
