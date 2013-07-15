80b9b107c4108b9eb281edcc6af1f839b8a62319
Sun Junwen <sunjw8888@gmail.com>
2013-07-15 21:15:33 +0800

    Fix when there is no gettext extension.

--- src/phpfm/inc/gettext.inc.php
+++ src/phpfm/inc/gettext.inc.php
@@ -15,9 +15,19 @@ $lang_dir = "locales";
 
 $directory = get_base_dir() . $lang_dir;
 
-bindtextdomain(DOMAIN, $directory);
-bind_textdomain_codeset(DOMAIN, get_encoding()); // 让 gettext 以 utf-8 读取 mo
-textdomain(DOMAIN);
+if(function_exists(bindtextdomain)) {
+	bindtextdomain(DOMAIN, $directory);
+	bind_textdomain_codeset(DOMAIN, get_encoding()); // 让 gettext 以 utf-8 读取 mo
+	textdomain(DOMAIN);
+} else {
+	function bindtextdomain($arg1, $arg2) {}
+	function bind_textdomain_codeset($arg1, $arg2){}
+	function textdomain($arg1){}
+
+	function _($str) {
+		return $str;
+	}
+}
 
 //echo _("Hello World!");
 
