6cdef1f0a44142c3e2616ea96e86e67b9160822d
Sun Junwen <sunjw8888@gmail.com>
2017-10-06 13:33:29 +0800

    Fix format.

--- src/phpfm/func/download.func.php
+++ src/phpfm/func/download.func.php
@@ -12,13 +12,13 @@ require_once "../log/log.func.php";
  */
 function dl_file($file)
 {
-    //Gather relevent info about file
-    $size = filesize($file);
-    $fileinfo['basename'] = get_basename($file);
+	//Gather relevent info about file
+	$size = filesize($file);
+	$fileinfo['basename'] = get_basename($file);
 
 	$fileinfo['extension'] = Utility::get_file_ext($file);
-    //workaround for IE filename bug with multiple periods / multiple dots in filename
-    //that adds square brackets to filename - eg. setup.abc.exe becomes setup[1].abc.exe
+	//workaround for IE filename bug with multiple periods / multiple dots in filename
+	//that adds square brackets to filename - eg. setup.abc.exe becomes setup[1].abc.exe
 	if(isset($_SERVER['HTTP_USER_AGENT']))
 	{
 		$filename = (strstr($_SERVER['HTTP_USER_AGENT'], 'MSIE')) ?
@@ -27,92 +27,92 @@ function dl_file($file)
 	}
 	else
 		$filename = $fileinfo['basename'];
-   
-    $file_extension = strtolower($fileinfo['extension']);
 
-//    switch($file_extension)
-//    {
-//        case 'exe': $ctype='application/octet-stream'; break;
-//        case 'zip': $ctype='application/zip'; break;
-//        case 'mp3': $ctype='audio/mpeg'; break;
-//        case 'mpg': $ctype='video/mpeg'; break;
-//        case 'avi': $ctype='video/x-msvideo'; break;
-//        default:    $ctype='application/force-download';
-//    }
+	$file_extension = strtolower($fileinfo['extension']);
+
+//	switch($file_extension)
+//	{
+//		case 'exe': $ctype='application/octet-stream'; break;
+//		case 'zip': $ctype='application/zip'; break;
+//		case 'mp3': $ctype='audio/mpeg'; break;
+//		case 'mpg': $ctype='video/mpeg'; break;
+//		case 'avi': $ctype='video/x-msvideo'; break;
+//		default: $ctype='application/force-download';
+//	}
 	$ctype = Utility::get_mime_type($file_extension);
 	//log_to_file($ctype);
 	//echo $ctype;
-    //check if http_range is sent by browser (or download manager)
-    
+	//check if http_range is sent by browser (or download manager)
+	
 	$range = '';
-    if(isset($_SERVER['HTTP_RANGE']))
-    {
-        list($size_unit, $range_orig) = explode('=', $_SERVER['HTTP_RANGE'], 2);
+	if(isset($_SERVER['HTTP_RANGE']))
+	{
+		list($size_unit, $range_orig) = explode('=', $_SERVER['HTTP_RANGE'], 2);
 		
-        if ($size_unit == 'bytes')
-        {
-            //multiple ranges could be specified at the same time, but for simplicity only serve the first range
-            //http://tools.ietf.org/id/draft-ietf-http-range-retrieval-00.txt
-            list($range, $extra_ranges) = explode(',', $range_orig, 2);
-        }
-        else
-        {
-            $range = '';
-        }
-    }
-    else
-    {
-        $range = '';
-    }
+		if ($size_unit == 'bytes')
+		{
+			//multiple ranges could be specified at the same time, but for simplicity only serve the first range
+			//http://tools.ietf.org/id/draft-ietf-http-range-retrieval-00.txt
+			list($range, $extra_ranges) = explode(',', $range_orig, 2);
+		}
+		else
+		{
+			$range = '';
+		}
+	}
+	else
+	{
+		$range = '';
+	}
 
-    //figure out download piece from range (if set)
+	//figure out download piece from range (if set)
 	if($range != '')
 		list($seek_start, $seek_end) = explode('-', $range, 2);
 
-    //set start and end based on range (if set), else set defaults
-    //also check for invalid ranges.
-    $seek_end = (empty($seek_end)) ? ($size - 1) : min(abs(intval($seek_end)),($size - 1));
-    $seek_start = (empty($seek_start) || $seek_end < abs(intval($seek_start))) ? 0 : max(abs(intval($seek_start)),0);
+	//set start and end based on range (if set), else set defaults
+	//also check for invalid ranges.
+	$seek_end = (empty($seek_end)) ? ($size - 1) : min(abs(intval($seek_end)),($size - 1));
+	$seek_start = (empty($seek_start) || $seek_end < abs(intval($seek_start))) ? 0 : max(abs(intval($seek_start)),0);
 
-    //add headers if resumable
-    //if ($is_resume)
-    //{
-        //Only send partial content header if downloading a piece of the file (IE workaround)
-    if ($seek_start > 0 || $seek_end < ($size - 1))
-    {
-        header('HTTP/1.1 206 Partial Content');
-    }
+	//add headers if resumable
+	//if ($is_resume)
+	//{
+		//Only send partial content header if downloading a piece of the file (IE workaround)
+	if ($seek_start > 0 || $seek_end < ($size - 1))
+	{
+		header('HTTP/1.1 206 Partial Content');
+	}
 
-    header('Accept-Ranges: bytes');
-    header('Content-Range: bytes '.$seek_start.'-'.$seek_end.'/'.$size);
-    //}
+	header('Accept-Ranges: bytes');
+	header('Content-Range: bytes '.$seek_start.'-'.$seek_end.'/'.$size);
+	//}
 
-    //headers for IE Bugs (is this necessary?)
-    //header("Cache-Control: cache, must-revalidate");  
-    //header("Pragma: public");
+	//headers for IE Bugs (is this necessary?)
+	//header("Cache-Control: cache, must-revalidate");  
+	//header("Pragma: public");
 
-    header('Content-Type: ' . $ctype);
-    header('Content-Disposition: attachment; filename="' . $filename . '"');
-    header('Content-Length: '.($seek_end - $seek_start + 1));
+	header('Content-Type: ' . $ctype);
+	header('Content-Disposition: attachment; filename="' . $filename . '"');
+	header('Content-Length: '.($seek_end - $seek_start + 1));
 
-    //open the file
-    $fp = fopen($file, 'rb');
-    //seek to start of missing part
-    fseek($fp, $seek_start);
+	//open the file
+	$fp = fopen($file, 'rb');
+	//seek to start of missing part
+	fseek($fp, $seek_start);
 
-    //start buffered download
-    while(!feof($fp))
-    {
-        //reset time limit for big files
-        set_time_limit(0);
-        print(fread($fp, 1024*8));
-        flush();
-        ob_flush();
-    }
+	//start buffered download
+	while(!feof($fp))
+	{
+		//reset time limit for big files
+		set_time_limit(0);
+		print(fread($fp, 1024*8));
+		flush();
+		ob_flush();
+	}
 
-    fclose($fp);
-    exit;
-    
+	fclose($fp);
+	exit;
+	
 }
 
 /**
@@ -193,7 +193,7 @@ if(Utility::allow_to_browser())
 		log_to_file("Download fail");
 		header("HTTP/1.1 404 Not Found");
 		echo "<strong>404 Not Found</strong>";
-	    exit;
+		exit;
 	}
 }
 //
