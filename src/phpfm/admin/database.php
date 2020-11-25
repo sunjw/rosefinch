ff30b4e2da1aed28f2c60475eaad484551a915ba
Sun Junwen <sunjw8888@gmail.com>
2020-11-25 19:01:19 +0800

    Code format.

--- src/phpfm/admin/database.php
+++ src/phpfm/admin/database.php
@@ -15,66 +15,59 @@ set_response_utf8();
 
 function check_table($db)
 {
-	$query = "SHOW TABLES";
-	$rows = $db->get_results($query, ARRAY_N);
-	//print_r($rows);
-	$has_fileindex = false;
-	$has_users = false;
-	if($rows != null)
-	{	
-		foreach($rows as $row)
-		{
-			//echo $row[0];
-			if($row[0] == "fileindex")
-			{
-				$has_fileindex = true;
-			}
-			if($row[0] == "users")
-			{
-				$has_users = true;
-			}
-		}
-	}
-	
-	return ($has_fileindex && $has_users);
+    $query = "SHOW TABLES";
+    $rows = $db->get_results($query, ARRAY_N);
+    //print_r($rows);
+    $has_fileindex = false;
+    $has_users = false;
+    if ($rows != null) {
+        foreach ($rows as $row) {
+            //echo $row[0];
+            if ($row[0] == "fileindex") {
+                $has_fileindex = true;
+            }
+            if ($row[0] == "users") {
+                $has_users = true;
+            }
+        }
+    }
+
+    return ($has_fileindex && $has_users);
 }
 
 function save_settings(&$settings)
 {
-	$settings['db_user'] = post_query("dbUser");
-	$settings['db_pswd'] = post_query("dbPswd");
-	$settings['db_name'] = post_query("dbName");
-	$settings['db_host'] = post_query("dbHost");
-	$settings['root_password'] = post_query("rootPassword");
-	
-	if($settings['db_user'] == "" ||
-		$settings['db_pswd'] == "" ||
-		$settings['db_name'] == "" ||
-		$settings['db_host'] == "" ||
-		$settings['root_password'] == "")
-	{
-		return false;
-	}
-	
-	// Initialise database object and establish a connection
-	// at the same time - db_user / db_password / db_name / db_host
-	$db = new ezSQL_mysqli($settings['db_user'], $settings['db_pswd'], $settings['db_name'], $settings['db_host']);
-	
-	$db->hide_errors();
-	
-	$query = "set names 'utf8'";
-	$ret = $db->query($query);
-	if((!is_numeric($ret) && !$ret))
-	{
-		return false; // Cannot connect to database
-	}
-	
-	$has_table = check_table($db);
+    $settings['db_user'] = post_query("dbUser");
+    $settings['db_pswd'] = post_query("dbPswd");
+    $settings['db_name'] = post_query("dbName");
+    $settings['db_host'] = post_query("dbHost");
+    $settings['root_password'] = post_query("rootPassword");
 
-	if(!$has_table)
-	{
-		// 表不存在，试着创建
-		$query = "CREATE TABLE IF NOT EXISTS `fileindex` (
+    if ($settings['db_user'] == "" ||
+        $settings['db_pswd'] == "" ||
+        $settings['db_name'] == "" ||
+        $settings['db_host'] == "" ||
+        $settings['root_password'] == "") {
+        return false;
+    }
+
+    // Initialise database object and establish a connection
+    // at the same time - db_user / db_password / db_name / db_host
+    $db = new ezSQL_mysqli($settings['db_user'], $settings['db_pswd'], $settings['db_name'], $settings['db_host']);
+
+    $db->hide_errors();
+
+    $query = "set names 'utf8'";
+    $ret = $db->query($query);
+    if ((!is_numeric($ret) && !$ret)) {
+        return false; // Cannot connect to database
+    }
+
+    $has_table = check_table($db);
+
+    if (!$has_table) {
+        // 表不存在，试着创建
+        $query = "CREATE TABLE IF NOT EXISTS `fileindex` (
 			  `path_hash` varchar(32) NOT NULL,
 			  `path` text NOT NULL,
 			  `name` varchar(255) NOT NULL,
@@ -87,9 +80,9 @@ function save_settings(&$settings)
 			  KEY `type` (`type`),
 			  KEY `size` (`size`)
 			) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
-		$db->query($query);
-		
-		$query = "CREATE TABLE IF NOT EXISTS `users` (
+        $db->query($query);
+
+        $query = "CREATE TABLE IF NOT EXISTS `users` (
 			  `id` int(10) unsigned NOT NULL auto_increment,
 			  `username` varchar(128) NOT NULL,
 			  `password` varchar(32) NOT NULL,
@@ -97,41 +90,40 @@ function save_settings(&$settings)
 			  PRIMARY KEY  (`id`),
 			  KEY `username` (`username`)
 			) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;";
-		$db->query($query);
-		
-		if(!check_table($db))
-		{
-			return false; // 数据库表添加失败
-		}
-		
-		$query = "INSERT INTO `users` SET username='root', password='".md5($settings['root_password'])."', permission=100";
-		$db->query($query);
-	}
-	
-	// 保存设置
-	$file_name = "database.inc.tpl";
-	$settings_tpl = fopen($file_name, "r");
-	$settings_str = fread($settings_tpl, filesize($file_name));
-	fclose($settings_tpl);
-	//print_r( $settings);
-	
-	$templates = array("&&DB_USER&&", 
-						"&&DB_PSWD&&", 
-						"&&DB_NAME&&",
-						"&&DB_HOST&&");
-	$values = array($settings['db_user'],
-					$settings['db_pswd'],
-					$settings['db_name'],
-					$settings['db_host']);
-			
-	$settings_str = str_replace($templates, $values, $settings_str);
-	//echo $settings;
-	
-	$settings_php = fopen("database.inc.php", "w");
-	fwrite($settings_php, $settings_str); // 写回配置文件
-	fclose($settings_php);
-	
-	return true;
+        $db->query($query);
+
+        if (!check_table($db)) {
+            return false; // 数据库表添加失败
+        }
+
+        $query = "INSERT INTO `users` SET username='root', password='" . md5($settings['root_password']) . "', permission=100";
+        $db->query($query);
+    }
+
+    // 保存设置
+    $file_name = "database.inc.tpl";
+    $settings_tpl = fopen($file_name, "r");
+    $settings_str = fread($settings_tpl, filesize($file_name));
+    fclose($settings_tpl);
+    //print_r( $settings);
+
+    $templates = array("&&DB_USER&&",
+        "&&DB_PSWD&&",
+        "&&DB_NAME&&",
+        "&&DB_HOST&&");
+    $values = array($settings['db_user'],
+        $settings['db_pswd'],
+        $settings['db_name'],
+        $settings['db_host']);
+
+    $settings_str = str_replace($templates, $values, $settings_str);
+    //echo $settings;
+
+    $settings_php = fopen("database.inc.php", "w");
+    fwrite($settings_php, $settings_str); // 写回配置文件
+    fclose($settings_php);
+
+    return true;
 }
 
 //print_r($_GET);
@@ -141,94 +133,97 @@ $settings = array();
 $ok = false;
 $display_msg = false;
 
-if(isset($_POST['settingsForm']))
-{
-	if(save_settings($settings))
-	{
-		redirect("setting.php");
-	}
-	$display_msg = true;
+if (isset($_POST['settingsForm'])) {
+    if (save_settings($settings)) {
+        redirect("setting.php");
+    }
+    $display_msg = true;
 }
 
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-	<title><?php echo _("Database Setting"); ?></title>
-	<meta http-equiv="Content-type" content="text/html;charset=UTF-8" />
-	<link href="../css/com.css" rel="stylesheet" type="text/css" />
-	<link href="../css/message.css" rel="stylesheet" type="text/css" />
-	<link href="../css/setting.css" rel="stylesheet" type="text/css" />
-	<script type="text/javascript" language="javascript" src="../js/jquery-1.8.1.min.js"></script>
-	<script type="text/javascript" language="javascript" src="../js/setting.js"></script>
+    <title><?php echo _("Database Setting"); ?></title>
+    <meta http-equiv="Content-type" content="text/html;charset=UTF-8"/>
+    <link href="../css/com.css" rel="stylesheet" type="text/css"/>
+    <link href="../css/message.css" rel="stylesheet" type="text/css"/>
+    <link href="../css/setting.css" rel="stylesheet" type="text/css"/>
+    <script type="text/javascript" language="javascript" src="../js/jquery-1.8.1.min.js"></script>
+    <script type="text/javascript" language="javascript" src="../js/setting.js"></script>
 </head>
 <body>
-	<div id="nav">
-		<?php Utility::html_navigation("setting"); ?>
-		<div class="clear"></div>
-	</div>
-	<div id="header">
-		<div id="mainTitle">
-			<?php echo _("Database Setting"); ?>
-		</div>
-		<div id="subTitle">
-			<?php echo _("Set database parameters"); ?>
-		</div>
-	</div>
-	<div id="content">
-		<div id="phpfmMessage" <?php if($display_msg)print("style='display:block' "); if($wrong)print("class='wrong' "); ?>>
-			<?php 
-			if(!$ok)
-				echo _("There is something wrong in your settings.");
-			else
-				echo "!!!!!";
-			?>
-		</div>
-		<form id="phpfmDatabaseForm" action="<?php echo get_URI(); ?>" method="post">
-			<input type="hidden" name="settingsForm" value="settingsForm" />
-			<fieldset>
-				<legend><?php echo _("Basic Settings"); ?></legend>
-				<div>
-					<label for="dbUser"><?php echo _("Database user name:"); ?></label>
-					<input id="dbUser" type="text" maxlength="256" value="<?php echo $settings['db_user']; ?>" name="dbUser"/>
-					<div class="info">
-						
-					</div>
-				</div>
-				<div>
-					<label for="dbPswd"><?php echo _("Database user password:"); ?></label>
-					<input id="dbPswd" type="password" maxlength="256" value="" name="dbPswd"/>
-					<div class="info">
-						
-					</div>
-				</div>
-				<div>
-					<label for="dbName"><?php echo _("Database name:"); ?></label>
-					<input id="dbName" type="text" maxlength="256" value="<?php echo $settings['db_name']; ?>" name="dbName"/>
-					<div class="info">
-						
-					</div>
-				</div>
-				<div>
-					<label for="dbHost"><?php echo _("Database host:"); ?></label>
-					<input id="dbHost" type="text" maxlength="256" value="<?php echo $settings['db_host']; ?>" name="dbHost"/>
-					<div class="info">
-						
-					</div>
-				</div>
-				<div>
-					<label for="rootPassword"><?php echo _("Root user password:"); ?></label>
-					<input id="rootPassword" type="password" maxlength="256" value="" name="rootPassword"/>
-					<div class="info">
-						
-					</div>
-				</div>
-			</fieldset>
-			<input type="submit" value="<?php echo _("OK"); ?>" />
-		</form>
-	</div>
-	<div id="footer">
-		<?php Utility::html_copyright_info($begin_time); ?>
-	</div>
+<div id="nav">
+    <?php Utility::html_navigation("setting"); ?>
+    <div class="clear"></div>
+</div>
+<div id="header">
+    <div id="mainTitle">
+        <?php echo _("Database Setting"); ?>
+    </div>
+    <div id="subTitle">
+        <?php echo _("Set database parameters"); ?>
+    </div>
+</div>
+<div id="content">
+    <div id="phpfmMessage" <?php if ($display_msg) print("style='display:block' ");
+    if ($wrong) print("class='wrong' "); ?>>
+        <?php
+        if (!$ok)
+            echo _("There is something wrong in your settings.");
+        else
+            echo "!!!!!";
+        ?>
+    </div>
+    <form id="phpfmDatabaseForm" action="<?php echo get_URI(); ?>" method="post">
+        <input type="hidden" name="settingsForm" value="settingsForm"/>
+        <fieldset>
+            <legend><?php echo _("Basic Settings"); ?></legend>
+            <div>
+                <label for="dbUser"><?php echo _("Database user name:"); ?></label>
+                <input id="dbUser" type="text" maxlength="256" value="<?php echo $settings['db_user']; ?>"
+                       name="dbUser"/>
+                <div class="info">
+
+                </div>
+            </div>
+            <div>
+                <label for="dbPswd"><?php echo _("Database user password:"); ?></label>
+                <input id="dbPswd" type="password" maxlength="256" value="" name="dbPswd"/>
+                <div class="info">
+
+                </div>
+            </div>
+            <div>
+                <label for="dbName"><?php echo _("Database name:"); ?></label>
+                <input id="dbName" type="text" maxlength="256" value="<?php echo $settings['db_name']; ?>"
+                       name="dbName"/>
+                <div class="info">
+
+                </div>
+            </div>
+            <div>
+                <label for="dbHost"><?php echo _("Database host:"); ?></label>
+                <input id="dbHost" type="text" maxlength="256" value="<?php echo $settings['db_host']; ?>"
+                       name="dbHost"/>
+                <div class="info">
+
+                </div>
+            </div>
+            <div>
+                <label for="rootPassword"><?php echo _("Root user password:"); ?></label>
+                <input id="rootPassword" type="password" maxlength="256" value="" name="rootPassword"/>
+                <div class="info">
+
+                </div>
+            </div>
+        </fieldset>
+        <input type="submit" value="<?php echo _("OK"); ?>"/>
+    </form>
+</div>
+<div id="footer">
+    <?php Utility::html_copyright_info($begin_time); ?>
+</div>
 </body>
 </html>
