da5dd2676f5fda539be6ee61ea856b087f9b3c47
Sun Junwen <sunjw8888@gmail.com>
2020-11-30 14:57:03 +0800

    Refactor logger.

--- src/phpfm/index.php
+++ src/phpfm/index.php
@@ -9,11 +9,12 @@ require_once "log/log.func.php";
 
 @session_start();
 
-log_to_file("index.php visited.");
+get_logger()->info("index.php visited.");
 
 $search_mode = false;
-if(get_query("q") != "")
+if (get_query("q") != "") {
     $search_mode = true;
+}
 
 $fileManager = new FileManager($search_mode, "index.php", "index.php");
 ?>
@@ -21,7 +22,7 @@ $fileManager = new FileManager($search_mode, "index.php", "index.php");
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <title>
-        <?php 
+        <?php
         if(!$search_mode)
             echo $fileManager->title()." - ".$fileManager->get_current_dir();
         else
@@ -40,17 +41,17 @@ $fileManager = new FileManager($search_mode, "index.php", "index.php");
             <div class="clear"></div>
         </div>
         <div id="loginStatus">
-        <?php 
+        <?php
         echo Utility::display_user();
         ?>
         </div>
     </div>
     <div id="content">
-        <?php 
+        <?php
         $fileManager->display_toolbar();
         $fileManager->display_full_path(); // 显示全路径
         $fileManager->display_main_view();
-        
+
         // 准备部分
         $fileManager->display_func_pre();
         ?>
--- src/phpfm/log/log.func.php
+++ src/phpfm/log/log.func.php
@@ -1,26 +1,18 @@
 <?php
 
-define("LOG_CODE", "UTF-8");
-define("LOG_FILE_NAME", "phpfm.log");
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
 
-/**
- * 将信息记录到日志文件
- * 日志文件位于 /log/LOG_FILE_NAME
- * @param $content 记录的内容
- */
-function log_to_file($content)
+define('LOG_CODE', 'UTF-8');
+define('LOG_FILE_NAME', 'phpfm.log');
+
+$logger = new Logger('phpfm');
+$logger->pushHandler(new StreamHandler(dirname(__FILE__) . LOG_FILE_NAME));
+
+function get_logger()
 {
-    $log_path = dirname(__FILE__); // 获得 log.php 文件夹的路径
-    $log_file = @fopen($log_path . "/" . LOG_FILE_NAME, "a+");
-    if ($log_file !== FALSE) {
-        $len = mb_strlen($content, LOG_CODE);
-        $last_char = mb_substr($content, $len - 1, 1, LOG_CODE);
-        if ($last_char != "\n") {
-            $content .= "\n";
-        }
-        fwrite($log_file, @date("Y-m-d H:i:s") . " " . $_SERVER['REMOTE_ADDR'] . ": " . $content);
-        fclose($log_file);
-    }
+    global $logger;
+    return $logger;
 }
 
 ?>
