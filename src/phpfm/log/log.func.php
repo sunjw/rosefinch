11b068d4ccc9f7db524b27ed127025a94bd2a993
Sun Junwen <sunjw8888@gmail.com>
2020-11-30 15:27:46 +0800

    Better logger format.

--- src/phpfm/log/log.func.php
+++ src/phpfm/log/log.func.php
@@ -1,13 +1,18 @@
 <?php
+require_once 'vendor\autoload.php';
 
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
+use Monolog\Formatter\LineFormatter;
 
 define('LOG_CODE', 'UTF-8');
 define('LOG_FILE_NAME', 'phpfm.log');
 
 $logger = new Logger('phpfm');
-$logger->pushHandler(new StreamHandler(dirname(__FILE__) . LOG_FILE_NAME));
+$log_file_path = dirname(__FILE__) . '/' . LOG_FILE_NAME;
+$stream_handler = new StreamHandler($log_file_path);
+$stream_handler->setFormatter(new LineFormatter(null, null, false, true));
+$logger->pushHandler($stream_handler);
 
 function get_logger()
 {
