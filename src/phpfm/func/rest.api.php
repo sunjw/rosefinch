7557be82523e3187b2b3759a345207cb65436826
Sun Junwen <sunjw8888@gmail.com>
2020-12-08 13:59:53 +0800

    Some refactor.

--- src/phpfm/func/rest.api.php
+++ src/phpfm/func/rest.api.php
@@ -1,7 +1,5 @@
 <?php
-require_once dirname(__FILE__) . "/../inc/defines.inc.php";
-require_once dirname(__FILE__) . "/../inc/common.inc.php";
-require_once dirname(__FILE__) . "/../clazz/rest.class.php";
+require_once dirname(__FILE__) . '/../clazz/rest.class.php';
 
 // For HTML5 upload.
 if (isset($_REQUEST['session']) && $_REQUEST['session']) {
