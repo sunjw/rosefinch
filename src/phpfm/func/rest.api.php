b81f6b38a398a6964909e67c089db9c0b665c00e
Sun Junwen <sunjw8888@gmail.com>
2020-12-08 10:52:56 +0800

    Some refactor.

--- src/phpfm/func/rest.api.php
+++ src/phpfm/func/rest.api.php
@@ -3,9 +3,10 @@ require_once dirname(__FILE__) . "/../inc/defines.inc.php";
 require_once dirname(__FILE__) . "/../inc/common.inc.php";
 require_once dirname(__FILE__) . "/../clazz/post.class.php";
 
-// For uploadify
-if (isset($_REQUEST['session']) && $_REQUEST['session'])
+// For HTML5 upload.
+if (isset($_REQUEST['session']) && $_REQUEST['session']) {
     @session_id($_REQUEST['session']);
+}
 
 @session_start();
 
