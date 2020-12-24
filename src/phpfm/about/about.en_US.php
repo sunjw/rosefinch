6dbe6cb23b90db973eef569077e64efde0e66a06
Sun Junwen <sunjw8888@gmail.com>
2020-12-24 11:01:23 +0800

    Some refactor.

--- src/phpfm/about/about.en_US.php
+++ src/phpfm/about/about.en_US.php
@@ -1,6 +1,6 @@
 <div id="phpfmDoc">
     <div id="phpfmDocReadme">
-        <a name="Readme" title="<?php echo _("Readme"); ?>"></a>
+        <a name="Readme" title="<?php echo _('Readme'); ?>"></a>
         <h4>Rosefinch is a web file manager written in php.</h4>
         <p>It provides features that you can manage files on local or remote computer. You can cut, copy, paste, rename and delete files or directories, and even make a new directory and upload a file (need higher user right). Of course, you can downlaod file from Rosefinch. Rosefich can provide search feature if there is a database server.</p>
         <h4>Rosefinch is platform independence and has multi-language feature.</h4>
@@ -12,16 +12,16 @@
         <p>Because of using javascript and ajax, you should allow browser to support javascript. Rosefinch uses Cookie to store user settings, you should allow browser to support Cookie. If you want to use a full function version of Rosefinch, please use IE 7 or newer version of IE browser, or use modern browsers like <strong>Firefox</strong>.</p>
     </div>
     <div id="phpfmDocLicence">
-        <a name="Licence" title="<?php echo _("Licence"); ?>"></a>
+        <a name="Licence" title="<?php echo _('Licence'); ?>"></a>
         <p>Rosefinch uses GPL 2.0 license.</p>
         <p>License: <a href="gpl-2.0.txt">GPL 2.0</a></p>
         <p>You can modify, redistribute and use this. But you MUST use GPL 2.0 license.</p>
         <p>Please contact author for other licenses.</p>
     </div>
     <div id="phpfmDocAbout">
-        <a name="About" title="<?php echo _("About"); ?>"></a>
+        <a name="About" title="<?php echo _('About'); ?>"></a>
         <h4>About</h4>
-        <?php printf("<p>Rosefinch - %s - PHPFM %s</p><p>%s</p>", _("Rosefinch"), VERSION, _("Using Google Android icons")); ?>
+        <?php printf("<p>Rosefinch - %s - PHPFM %s</p><p>%s</p>", _('Rosefinch'), VERSION, _('Using Google Android icons')); ?>
         <h4>Author</h4>
         <p>Sun Junwen</p>
     </div>
--- src/phpfm/about/about.zh_CN.php
+++ src/phpfm/about/about.zh_CN.php
@@ -1,6 +1,6 @@
 <div id="phpfmDoc">
     <div id="phpfmDocReadme">
-        <a name="Readme" title="<?php echo _("Readme"); ?>"></a>
+        <a name="Readme" title="<?php echo _('Readme'); ?>"></a>
         <h4>Rosefinch (朱雀) 是一个用 php 编写的 Web 文件管理程序。</h4>
         <p>它提供了对于远程服务器或本地文件的管理能力，可以对文件或目录进行剪切、复制、粘贴、重命名和删除操作，还可以新建目录以及上传文件 (以上操作均需要对服务器端文件操作权限进行适当的设置)。当然，也可以下载文件，并且支持断点续传或客户端多线程下载。它还可以在数据库的支持下提供搜索文件的功能。</p>
         <h4>Rosefinch (朱雀) 拥有跨平台、多语言的特性。</h4>
@@ -12,16 +12,16 @@
         <p>由于使用到 javascript 和 ajax 技术，所以需要浏览器支持 javascript。Rosefinch (朱雀) 通过 Cookie 记录下用户的一些设置 (如：排序方法)，所以需要浏览器能够记录 Cookie。如果希望功能保持完整并得到最佳的界面效果，使用 IE 7 及以上版本的 IE 浏览器，或者<strong>最好</strong>使用诸如 Firefox 之类的现代浏览器。</p>
     </div>
     <div id="phpfmDocLicence">
-        <a name="Licence" title="<?php echo _("Licence"); ?>"></a>
+        <a name="Licence" title="<?php echo _('Licence'); ?>"></a>
         <p>Rosefinch (朱雀) 使用 GPL 2.0 协议授权。</p>
         <p>协议文本: <a href="gpl-2.0.txt">GPL 2.0</a></p>
         <p>您可以修改代码，再分发以及其他用途，但必须遵守 GPL 2.0 协议。</p>
         <p>其他授权，请联系作者。</p>
     </div>
     <div id="phpfmDocAbout">
-        <a name="About" title="<?php echo _("About"); ?>"></a>
+        <a name="About" title="<?php echo _('About'); ?>"></a>
         <h4>关于</h4>
-        <?php printf("<p>Rosefinch - %s - PHPFM %s</p><p>%s</p>", _("Rosefinch"), VERSION, _("Using Google Android icons")); ?>
+        <?php printf("<p>Rosefinch - %s - PHPFM %s</p><p>%s</p>", _('Rosefinch'), VERSION, _('Using Google Android icons')); ?>
         <h4>作者</h4>
         <p>Sun Junwen</p>
     </div>
