1e5f1efecabc50f2fd1668a3ac8f7a2f0eac8f01
Sun Junwen <sunjw8888@gmail.com>
2020-12-29 15:25:50 +0800

    Add CopyPlugin to copy config file.

--- src/phpfm_fed/webpack.config.js
+++ src/phpfm_fed/webpack.config.js
@@ -1,7 +1,17 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+
 // Extract CSS
 const extractCSS = new MiniCssExtractPlugin({
   filename: 'styles.min.css'
+});
+
+// Copy file
+const copyPlugin = new CopyPlugin({
+  patterns: [{
+      from: "phpfm.config.js",
+      to: "",
+    }],
 });
 
 module.exports = {
@@ -41,6 +51,7 @@ module.exports = {
     ]
   },
   plugins: [
-    extractCSS
+    extractCSS,
+    copyPlugin
   ]
 }
