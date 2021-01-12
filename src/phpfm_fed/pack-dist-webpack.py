073e53f8aa69a44cf1bb8b6a424147aa837c8e86
Sun Junwen <sunjw8888@gmail.com>
2021-01-12 19:19:41 +0800

    fed: Update package script.

--- src/phpfm_fed/pack-dist-webpack.py
+++ src/phpfm_fed/pack-dist-webpack.py
@@ -9,23 +9,23 @@ from subprocess import Popen, PIPE, STDOUT
 
 # Log to stdout
 def log(message):
-    print(message)
+    print('%s' % (message))
 
 # Log to stderr
 def log_err(message):
     print(message, file=sys.stderr)
 
 def run_cmd(cmd):
-    log("Run: \n%s" % (cmd))
+    log('Run: \n%s' % (cmd))
     os.system(cmd)
 
 def run_cmd_with_stdio(cmd, input_data):
-    log("Run: \n%s" % (cmd))
+    log('Run: \n%s' % (cmd))
     p = Popen(shlex.split(cmd), stdout=PIPE, stdin=PIPE, stderr=STDOUT)
     return p.communicate(input = input_data)[0]
 
 def run_cmd_with_stderr(cmd):
-    log("Run: \n%s" % (cmd))
+    log('Run: \n%s' % (cmd))
     p = Popen(shlex.split(cmd), stdout=PIPE, stdin=PIPE, stderr=PIPE)
 
     stdout_text, stderr_text = p.communicate()
@@ -33,11 +33,14 @@ def run_cmd_with_stderr(cmd):
     return stderr_text
 
 def is_windows_sys():
-    return (platform.system() == "Windows")
+    return (platform.system() == 'Windows')
+
+def is_macos_sys():
+    return (platform.system() == 'Darwin')
 
 def fix_win_path(path):
     if is_windows_sys():
-        return path.replace("/", "\\")
+        return path.replace('/', '\\')
     else:
         return path
 
@@ -70,6 +73,17 @@ def remove_file(path):
 def remove_dir(path):
     if os.path.exists(path):
         shutil.rmtree(path)
+
+def read_file_content(file_path):
+    if not os.path.exists(file_path):
+        return ''
+    file_content = open(file_path, 'rb').read()
+    return file_content
+
+def write_file_content(file_path, file_content):
+    file_obj = open(file_path, 'wb')
+    file_obj.write(file_content)
+    file_obj.close()
 
 def log_stage(stage_message):
     log('\n%s\n' % (stage_message))
@@ -105,6 +119,13 @@ def main():
     for app_file in app_files:
         dest_app_file = os.path.join(publish_dir, app_file)
         copy_file(app_file, dest_app_file)
+
+    # Clear up.
+    clear_files = []
+    for clear_file in clear_files:
+        dest_clear_file = os.path.join(publish_dir, clear_file)
+        if os.path.exists(dest_clear_file):
+            os.remove(dest_clear_file)
 
 if __name__ == '__main__':
     main()
