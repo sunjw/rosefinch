ed4e0cb8728f4a48a039b8aa80131ffb70588945
Sun Junwen <sunjw8888@gmail.com>
2021-03-16 22:56:36 +0800

    fed: Tweak image preview.

--- src/phpfm_fed/js/main.js
+++ src/phpfm_fed/js/main.js
@@ -1133,7 +1133,12 @@ class RosefinchPage {
                 utils.log('RosefinchPage.showImagePreviewDialog, calcPreviewSize, imgWidth=%d, imgHeight=%d, imgRatio=%f',
                     imgWidth, imgHeight, imgRatio);
 
-                let imgPreviewWidth = that.getWindowWidth() - 160;
+                let imgPreviewWidth = that.getWindowWidth();
+                if (imgPreviewWidth < 576) {
+                    imgPreviewWidth = imgPreviewWidth - 90;
+                } else {
+                    imgPreviewWidth = imgPreviewWidth - 120;
+                }
                 let imgPreviewHeight = that.getWindowHeight() - 300;
                 let imgPreviewRadio = imgPreviewWidth / imgPreviewHeight;
 
