750806b944d6aa698c5564647de0157768851af1
Sun Junwen <sunjw8888@gmail.com>
2021-12-01 13:47:57 +0800

    api: Clear gettext.

--- src/phpfm_api/inc/gettext.inc.php
+++ src/phpfm_api/inc/gettext.inc.php
@@ -1,38 +1,16 @@
 <?php
 
-require_once 'defines.inc.php';
-require_once 'common.inc.php';
-
-/*
- * Init gettext
- */
-if (!defined('LOCALE')) {
-    define('LOCALE', 'en-us');
-}
-putenv('LANG=' . LOCALE);
-setlocale(LC_ALL, LOCALE);
-
-$lang_dir = 'locales';
-
-$directory = get_base_dir() . $lang_dir;
-
-if (function_exists('bindtextdomain')) {
-    bindtextdomain(DOMAIN, $directory);
-    bind_textdomain_codeset(DOMAIN, get_encoding()); // Let gettext read mo by utf-8.
-    textdomain(DOMAIN);
-} else {
-    function bindtextdomain($arg1, $arg2) {}
-
-    function bind_textdomain_codeset($arg1, $arg2) {}
-
-    function textdomain($arg1) {}
-
-    function _($str)
-    {
-        return $str;
-    }
+function bindtextdomain($arg1, $arg2) {
 }
 
-//echo _('Hello World!');
+function bind_textdomain_codeset($arg1, $arg2) {
+}
+
+function textdomain($arg1) {
+}
+
+function _($str) {
+    return $str;
+}
 
 ?>
