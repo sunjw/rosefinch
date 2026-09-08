fc2e9ae81c22d868a9e381e540c5b78ee4d9fa83
Sun Junwen <sunjw8888@gmail.com>
2026-09-08 14:50:15 +0800

    fed: Update package script.

--- src/phpfm_fed/pack_dist_webpack.py
+++ src/phpfm_fed/pack_dist_webpack.py
@@ -1,9 +1,10 @@
 import os
 import platform
+import re
 import shlex
 import shutil
-import stat
 import sys
+import time
 
 from subprocess import Popen, PIPE, STDOUT
 
@@ -100,6 +101,7 @@ def run_cmd_with_stderr(cmd):
 
     return stdout_text, stderr_text
 
+
 PUBLISH_DIR = 'publish'
 
 APP_DIRS = ['dist']
@@ -107,6 +109,19 @@ APP_FILES = ['index.html']
 CLEAR_FILES = []
 CONFIG_PUBLISH_FILE = 'phpfm.config.publish.js'
 CONFIG_PUBLISH_DIST_FILE = 'dist/phpfm.config.js'
+CACHE_BUSTER_FILES = ['index.html']
+
+
+def add_cache_buster(html_content):
+    # Append "?v=<cache_buster>" to HTML content.
+    cache_buster = '%d' % int(time.time())
+    pattern = re.compile(r'((?:href|src))="(\./dist/[^"?]+)"')
+    def replace_func(match):
+        attr = match.group(1)
+        url = match.group(2)
+        return '%s="%s?v=%s"' % (attr, url, cache_buster)
+    return pattern.sub(replace_func, html_content)
+
 
 def main():
     cwd = os.getcwd()
@@ -146,9 +161,20 @@ def main():
 
     # Post step.
     log_stage('Post step...')
+
     config_dist_path = os.path.join(PUBLISH_DIR, CONFIG_PUBLISH_DIST_FILE)
     config_publish_content = read_file_binary(CONFIG_PUBLISH_FILE)
     write_file_binary(config_dist_path, config_publish_content)
+
+    for cache_buster_file in CACHE_BUSTER_FILES:
+        publish_path = os.path.join(PUBLISH_DIR, cache_buster_file)
+        if not os.path.exists(publish_path):
+            continue
+        content_bytes = read_file_binary(publish_path)
+        content = content_bytes.decode('utf-8')
+        content_buster = add_cache_buster(content)
+        log('Add cache buster to [%s]' % publish_path)
+        write_file_binary(publish_path, content_buster.encode('utf-8'))
 
 if __name__ == '__main__':
     main()
